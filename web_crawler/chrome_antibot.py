3be1a2828705168bbb53fad49cb7c59aa6add0da
Hosun-S <songhn@uci.edu>
2024-07-15 15:25:25 -0700

    base for parsing the products, need to implement dictionary still

--- web_crawler/chrome_antibot.py
+++ web_crawler/chrome_antibot.py
@@ -1,68 +1,3 @@
-# from bs4 import BeautifulSoup
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# import time
-# import random
-# from fake_useragent import UserAgent
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.action_chains import ActionChains
-
-
-# def get_random_user_agent():
-#     ua = UserAgent()
-#     return ua.random
-
-# def get_driver():
-#     options = Options()
-#     # options.add_argument("--headless")
-#     options.add_argument(f"user-agent={get_random_user_agent()}")
-#     options.add_argument("--enable-javascript")
-#     options.add_argument("--disable-blink-features=AutomationControlled") 
-#     options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
-#     options.add_experimental_option("useAutomationExtension", False) 
-
-#     driver = webdriver.Chrome(options=options)
-#     return driver
-
-
-
-# def fetching_ingredient(file):
-#     file2 = "products.html"
-#     with open(file, "r") as f:
-#         with open(file2, "w") as f2:
-#             line = f.readline()
-#             while line:
-#                 driver = get_driver()
-#                 driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})") 
-#                 driver.get(line)
-
-#                 time.sleep(random.uniform(1,5))
-
-#                 try:
-#                     WebDriverWait(driver, 10).until(
-#                         EC.presence_of_element_located((By.TAG_NAME, "body"))
-#                     )
-#                 except:
-#                     time.sleep(20)
-                    
-#                 soup = BeautifulSoup(driver.page_source, 'html.parser')
-#                 tb = soup.find_all("td",class_="data")
-
-#                 print(tb)
-#                 f2.write(str(tb) + "\n")
-                    
-#                 line = f.readline()
-#         driver.quit()
-
-# def main():
-#     file = "url_only.txt"
-#     fetching_ingredient(file)
-
-# if __name__ == '__main__':
-#     main()
-
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -85,22 +20,46 @@ def get_driver():
     return driver
 
 def fetching_ingredient(file):
-    file2 = "products.html"
+    file2 = "product.html"
+    product_dict = {}
     with open(file, "r") as f:
         with open(file2, "w") as f2:
             line = f.readline()
             while line:
+                line_ = line.split(' ')
+                url = line_[0]
+                title = "".join(line_[1:])
+
                 driver = get_driver()
-                driver.get(line)
+                driver.get(url)
                 time.sleep(random.uniform(3, 7))  # More random sleep time
                 soup = BeautifulSoup(driver.page_source, 'html.parser')
                 tb = soup.find_all("td", class_="data")
-                f2.write(str(tb))
+                ingredients = []
+                for td in tb:
+                    if td.find('b') or td.find('strong'):
+                        continue
+                    # Extract ingredient name
+                    ingredient_name = td.get_text(strip=True)
+                    amount_per_serving = ""
+
+                    next_td = td.find_next_sibling("td", class_="data")
+                    if next_td:
+                        amount_per_serving = next_td.get_text(strip=True)
+                    
+                    if ingredient_name:
+                        ingredients.append(f"{ingredient_name}: {amount_per_serving}")
+                
+                print(ingredients)
+                # f2.write(url + " " + title + '\n')
+                # f2.write(str(tb))
+                # f2.write('\n\n')
                 driver.quit()
                 line = f.readline()
+            f2.write(product_dict)
 
 def main():
-    file = "url_only.txt"
+    file = "all_url.txt"
     fetching_ingredient(file)
 
 if __name__ == '__main__':
